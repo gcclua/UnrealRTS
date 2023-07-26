0d11dfd97c404cb7a9731866ceb9e79f7c136321
errol <errolflynn8@hotmail.com>
2023-07-26 21:16:08 +1000

    Started minimap

---
+++ Source/TowerDefense/MiniMap.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "MiniMap.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TOWERDEFENSE_API UMiniMap : public UUserWidget
+{
+	GENERATED_BODY()
+	
+};
