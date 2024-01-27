b90f1feca47c908b7186af81db4dd83d79495cda
Errol McFadden <errolflynn8@hotmail.com>
2024-01-28 00:14:01 +1000

    Updated Build target version to match engine version + Fixed lighting in main level

--- Source/TowerDefenseEditor.Target.cs
+++ Source/TowerDefenseEditor.Target.cs
@@ -9,7 +9,7 @@ public class TowerDefenseEditorTarget : TargetRules
 	{
 		Type = TargetType.Editor;
 		DefaultBuildSettings = BuildSettingsVersion.V2;
-		IncludeOrderVersion = EngineIncludeOrderVersion.Unreal5_1;
+		IncludeOrderVersion = EngineIncludeOrderVersion.Unreal5_3;
 		ExtraModuleNames.Add("TowerDefense");
 	}
 }
